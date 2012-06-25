c5255ec385309c1e549fad0a7058c3514052898d
Carlos Hernandez <c.hernandez.guirado@gmail.com>
2012-06-25 16:35:23 +0200

    Nuevo commit

---
+++ GitSample/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitSample
+{
+    public class Car
+    {
+        public int IdCar { get; set; }
+        public string  Description { get; set; }
+        public Marca Marca { get; set; }
+        public Tipo Tipo { get; set; }
+    }
+
+    public enum Marca
+    {
+        Ferrari,
+        Renault,
+        Opel,
+        Mercedes
+    }
+
+    public enum Tipo
+    {
+        Utilitario,
+        Camion,
+        Motocicleta,
+        Modificacion
+    }
+}
---
+++ GitSample/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitSample
+{
+    public class Person
+    {
+        public int IdPerson { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Modificacion { get; set; }
+    }
+}
